894f18804e0b52b3a4a58fbe85934998b35c47c3
JadKaedBey <jkaedbey@gmail.com>
2021-03-22 21:43:23 +0200

    hi

---
+++ app/src/main/java/com/example/crisisopp/FarahFoundation/FarahFoundationMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.crisisopp.FarahFoundation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.crisisopp.R
+
+class FarahFoundationMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_farah_foundation_main)
+    }
+}
