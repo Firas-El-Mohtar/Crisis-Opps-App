b5a7f261da81b1871df5045a2cb0537c5e4ce228
JadKaedBey <jkaedbey@gmail.com>
2021-05-01 20:56:09 +0300

    Revert "added functions that allow user to send images to a collection in the fire base called"
    This reverts commit 335ae32a091b942a572ebd2669302e03c184c301.
    
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/crisisopp/LocalMunicipality/CreateForm.kt
    #	app/src/main/res/layout/fragment_create_form.xml

---
+++ app/src/main/java/com/example/crisisopp/LocalMunicipality/CreateForm.kt
@@ -0,0 +1,38 @@
+package com.example.crisisopp.LocalMunicipality
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.crisisopp.R
+
+class CreateForm : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_create_form, container, false)
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.let {
+            val width = ViewGroup.LayoutParams.MATCH_PARENT
+            val height = ViewGroup.LayoutParams.MATCH_PARENT
+            it.window?.setLayout(width, height)
+        }
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+//        setWidthPercent(85)
+    }
+
+
+}
