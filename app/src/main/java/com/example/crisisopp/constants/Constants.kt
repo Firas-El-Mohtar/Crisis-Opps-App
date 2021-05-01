f896f5bc0568a955f8c292faa7290fbc1c0a14c6
Jad Kaedbey <73882495+JadKaedBey@users.noreply.github.com>
2021-05-01 20:09:03 +0300

    Update Constants.kt

--- app/src/main/java/com/example/crisisopp/constants/Constants.kt
+++ app/src/main/java/com/example/crisisopp/constants/Constants.kt
@@ -7,7 +7,7 @@ class Constants {
      */
     companion object {
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAqju6Kfc:APA91bF8D01SXHzoiIjQfFdAH_4vqBHV6LMPIpex10JwXbeRxymcgtWQwlRV8OJP-HLu8uB95wSR5vNXcxuGM8ed1BjRWCBPLSOI8ktdRm2XvHVdizQtNhc5G0dk4YTQy7a1ZPNyYaob"
+        const val SERVER_KEY = "Contact us for API key"
         const val CONTENT_TYPE = "application/json"
     }
 }
